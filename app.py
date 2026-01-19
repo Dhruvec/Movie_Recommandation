55bf93d98009376b2f3882f1a2760b1e21f94025
Dhruvec <dhruvcc17745@gmail.com>
2026-01-19 18:59:52 +0530

    main

---
+++ app.py
@@ -0,0 +1,94 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import requests
+
+def fetch_poster(movies_id):
+    response = requests.get("https://api.themoviedb.org/3/movie/{}?api_key=951133355008d227d58352c692d47a31&language=en-US".format(movies_id))
+    data = response.json()
+    return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
+
+def recommend(movie):
+    movie_index = movies[movies["title"] == movie].index[0]
+    distances = similarity[movie_index]
+    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+    
+    recommended_movies = []
+    recommended_movies_poster = []
+    for i in movies_list:
+        movie_id = movies.iloc[i[0]].movie_id
+        recommended_movies.append(movies.iloc[i[0]].title)
+        recommended_movies_poster.append(fetch_poster(movie_id))
+    return recommended_movies, recommended_movies_poster
+
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-image: url('https://i.pinimg.com/736x/af/f0/6f/aff06fb3660a32e8f90bde5c46d3c80f.jpg');
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        background-position: center;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+st.markdown(
+    """
+    <div style="background-color: rgba(255, 255, 255, 0.7); padding: 10px; border-radius: 10px; display: inline-block;">
+        <h1 style="color: black; margin: 0;">Movie Recommender System</h1>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+
+movies_dict = pickle.load(open("movies_dict.pkl", "rb"))
+movies = pd.DataFrame(movies_dict)
+similarity = pickle.load(open("similarity.pkl", "rb"))
+
+selected_movie_name = st.selectbox(
+    'Select a movie',
+    movies["title"].values
+)
+
+col_button1, col_button2 = st.columns([1, 1])
+
+with col_button1:
+    recommend_clicked = st.button("Recommend")
+
+with col_button2:
+    generic_clicked = st.button("Category")
+
+if recommend_clicked:
+    names, posters = recommend(selected_movie_name)
+    
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1:
+        st.markdown(f"**{names[0]}**")
+        st.image(posters[0])
+    with col2:
+        st.markdown(f"**{names[1]}**")
+        st.image(posters[1])
+    with col3:
+        st.markdown(f"**{names[2]}**")
+        st.image(posters[2])   
+    with col4:
+        st.markdown(f"**{names[3]}**")
+        st.image(posters[3]) 
+    with col5:
+        st.markdown(f"**{names[4]}**")
+        st.image(posters[4])
+
+if generic_clicked:
+    generic_movies = movies.sample(5)
+    col1, col2, col3, col4, col5 = st.columns(5)
+    for idx, col in enumerate([col1, col2, col3, col4, col5]):
+        movie_title = generic_movies.iloc[idx].title
+        movie_id = generic_movies.iloc[idx].movie_id
+        poster_url = fetch_poster(movie_id)
+        with col:
+            st.markdown(f"**{movie_title}**")
+            st.image(poster_url)
